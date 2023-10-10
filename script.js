de255d56c00dbee3dd438b5fc90708849f9c2da5
anbukkannan6 <anbukkannan6@gmail.com>
2023-10-10 14:53:50 +0530

    clear button added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // let btn = document.querySelectorAll(".btn")
 let input = document.getElementById("input")
 
-let show = (num)=>{
-    input.value = input.value+num
-   console.log(typeof(num));
-   console.log(eval("2"+"4"));
+let show = (num) => {
+    input.value = input.value + num
+    console.log(typeof (num));
+    console.log(eval("2" + "4"));
 }
 
 
@@ -12,12 +12,12 @@ let show = (num)=>{
 
 let result = () => {
     console.log(input.value);
-    console.log(typeof(input.value));
+    console.log(typeof (input.value));
 
     input.value = eval(input.value);
 }
 
-let clear = () =>{  
+let clear = () => {
     input.value = " "
     console.log("Ima Working")
 }
