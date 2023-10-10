25ad1cd8cb3768242a7f230eec2f99c78b18777f
anbukkannan6 <anbukkannan6@gmail.com>
2023-10-10 14:45:01 +0530

    Calculator Created

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// let btn = document.querySelectorAll(".btn")
+let input = document.getElementById("input")
 
+let show = (num)=>{
+    input.value = input.value+num
+   console.log(typeof(num));
+   console.log(eval("2"+"4"));
+}
+
+
+
+
+let result = () => {
+    console.log(input.value);
+    console.log(typeof(input.value));
+
+    input.value = eval(input.value);
+}
+
+let clear = () =>{  
+    input.value = " "
+    console.log("Ima Working")
+}
